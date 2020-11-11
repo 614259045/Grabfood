b9d0c6919d7e5c1b4665f6ecb2f13f404b5f1ab2
Wantana  Wankhum <614259045@webmail.npru.ac.th>
2020-11-11 12:36:11 +0700

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const hbs = require('express-handlebars');
 const restaurantRouter = require('./routes/restaurant');
+
+const path = require('path');
 
 //creater Server
 const app = express();
 
-//Use Router
-app.use('/api', restaurantRouter);
+/*app.get('/',(req,res) => {
+    res.send('<h1>Hello Restaurant API <h1>');
+});*/
 
-app.listen(3000, ()=>{
-    console.log('Listening to post 3000');
-});
+//Template engine setting
+app.engine('hbs', hbs({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+//Middleware เพื่ออ่าน req.body
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+
+
+
+
+//router
+app.use('/', restaurantRouter);
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(
+    PORT,
+    () => {
+        console.log(`Listening to port ${PORT}`);
+    }
+);
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,38 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const restaurants = require('../data')
+const restaurants = require('../data');
 
 
-router.get('/' , (req,res) => {
-    res.send('<h1>Hello Restaurant API<h1>');
+//http://localhost:3000/api
+/*router.get('/',(req,res) => {
+    res.send('<h1> Hello Restaurant API</h1>');
+});*/
+
+//http://localhost:3000/api/restaurant
+router.get("/", (req, res) => {
+    //res.json(restaurants);
+    res.render('restaurant', {restaurants});
 });
 
-
-router.get("/restaurants", (req,res) =>{
-    res.json(restaurants);
-});
-
-
-router.get("/restaurants/:id", (req,res) =>{
-    const restaurantId = Number.parseInt(req.params.id,10);
+//http://localhost:3000/api/restaurant/1
+router.get("/restaurant/:id", (req, res) => {
+    const restaurantId = Number.parseInt(req.params.id, 10);
     const restaurant = restaurants.find((restaurant) => restaurant.id === restaurantId);
-  res.send(restaurant);
+    res.json(restaurant);
+});
+
+//http://localhost:3000/api/restaurant
+router.post("/restaurant", (req, res) => {
+    const newRestaurant = {
+        ...req.body
+    };
+    restaurants.push(newRestaurant);
+    res.json(newRestaurant);
 
 });
 
+//http://localhost:3000/api/restaurant/1
+router.put("/restaurant/:id", (req, res) => {
+    const restaurantId = Number.parseInt(req.params.id, 10);
+    const restaurantIndex = restaurants.findIndex((restaurant) => restaurant.id === restaurantId);
+    const updatedRestaurant = {
+        id: restaurantId,
+        ...req.body
+    };
+    restaurants[restaurantIndex] = updatedRestaurant;
+    res.json(updatedRestaurant);
 
-router.post("/restaurants", (req,res) =>{
-    res.send("POST API");
 });
 
-
-router.put("/restaurants/:id", (req,res) =>{
-    res.send("PUT API ");
-});
-
-
-router.delete("/restaurants/:id", (req,res) =>{
-    res.send("DELETE API ");
+//http://localhost:3000/api/restaurant/1
+router.delete("/restaurant/:id", (req, res) => {
+    const restaurantId = Number.parseInt(req.params.id, 10);
+    const restaurantIndex = restaurants.findIndex((restaurant) => restaurant.id === restaurantId);
+    restaurants.splice(restaurantIndex, 1);
+    res.sendStatus(204);
 });
 
 module.exports = router;
